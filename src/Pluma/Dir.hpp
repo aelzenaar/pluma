65f53bc73b5eb098b5ecac34b4a126595f92e40a
gsaurus <gsaurus@9a2b97de-1d9d-4232-b70c-25b35a4c34f7>
2012-03-13 23:26:35 +0000

    directory convention
    git-svn-id: svn://svn.code.sf.net/p/pluma-framework/code/trunk@18 9a2b97de-1d9d-4232-b70c-25b35a4c34f7

--- src/Pluma/Dir.hpp
+++ src/Pluma/Dir.hpp
@@ -1,7 +1,7 @@
 ////////////////////////////////////////////////////////////
 //
 // Pluma - Plug-in Management Framework
-// Copyright (C) 2010-2011 Gil Costa (gsaurus@gmail.com)
+// Copyright (C) 2010-2012 Gil Costa (gsaurus@gmail.com)
 //
 // This software is provided 'as-is', without any express or implied warranty.
 // In no event will the authors be held liable for any damages arising from the use of this software.
